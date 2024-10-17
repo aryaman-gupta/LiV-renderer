3705cb94611b0a53990ee677042111b6f0d3ce00
Aryaman Gupta <aryaman.gupta@tu-dresden.de>
2024-10-17 16:28:51 +0200

    DistributedRenderer: adds mpiParameters, corrects texture fetching before distribute

--- src/main/kotlin/graphics/scenery/DistributedRenderer.kt
+++ src/main/kotlin/graphics/scenery/DistributedRenderer.kt
@@ -1,10 +1,24 @@
 package graphics.scenery
 
+import graphics.scenery.textures.Texture
 import graphics.scenery.utils.extensions.fetchFromGPU
 import graphics.scenery.utils.lazyLogger
 import java.nio.ByteBuffer
 
-abstract class DistributedRenderer(var volumeManagerManager: VolumeManagerManager) {
+/**
+ * Data class representing MPI (Message Passing Interface) parameters.
+ *
+ * @property rank The rank of the current process.
+ * @property commSize The total number of processes in the communicator.
+ * @property nodeRank The rank of the process within its node.
+ */
+data class MPIParameters(
+    val rank: Int,
+    val commSize: Int,
+    val nodeRank: Int
+)
+
+abstract class DistributedRenderer(var volumeManagerManager: VolumeManagerManager, val mpiParameters: MPIParameters) {
 
     val logger by lazyLogger()
 
@@ -21,7 +35,7 @@ abstract class DistributedRenderer(var volumeManagerManager: VolumeManagerManage
         val textureFetched = firstPassTexture.fetchFromGPU()
 
         if (!textureFetched) {
-            logger.error("Error fetching first pass texture.")
+            throw RuntimeException("Error fetching first pass texture.").also { it.printStackTrace() }
         }
 
         return firstPassTexture.contents!!
@@ -33,8 +47,12 @@ abstract class DistributedRenderer(var volumeManagerManager: VolumeManagerManage
         return emptyList()
     }
 
-    open fun preProcess(process: () -> Unit) {
-        process()
+    fun preProcessBeforeDistribute(process: () -> Unit) {
+        try {
+            process()
+        } catch (e: Exception) {
+            logger.error("Error in pre-processing before distribution: ${e.message}")
+        }
     }
 
     abstract fun distributeForCompositing(buffers: List<ByteBuffer>)
@@ -55,15 +73,27 @@ abstract class DistributedRenderer(var volumeManagerManager: VolumeManagerManage
 
             val buffersToDistribute: MutableList<ByteBuffer> = mutableListOf()
             val colorTexture = volumeManagerManager.getColorTextureOrNull()!!
+            var textureFetched = colorTexture.fetchFromGPU()
+            if (!textureFetched) {
+                throw RuntimeException("Error fetching color texture.").also { it.printStackTrace() }
+            }
+
             buffersToDistribute.add(colorTexture.contents!!)
 
             // safe to assume that a 2-pass approach will always have a depth texture
             val depthTexture = volumeManagerManager.getDepthTextureOrNull()!!
+            textureFetched = depthTexture.fetchFromGPU()
+            if (!textureFetched) {
+                throw RuntimeException("Error fetching depth texture.").also { it.printStackTrace() }
+            }
+
             buffersToDistribute.add(depthTexture.contents!!)
 
             fetchAdditionalTextureData().forEach {
                 buffersToDistribute.add(it)
             }
+
+            distributeForCompositing(buffersToDistribute)
         }
     }
 }
