96ccb670b273f4e84d4cc48e4b899b9bdfff98dd
Aryaman Gupta <aryaman.gupta@tu-dresden.de>
2024-10-23 15:23:57 +0200

    DistributedVDIs: implements overridden uploadForCompositing and setCompositingActivityStatus fns

--- src/main/kotlin/graphics/scenery/parallelization/DistributedVDIs.kt
+++ src/main/kotlin/graphics/scenery/parallelization/DistributedVDIs.kt
@@ -6,10 +6,13 @@ import graphics.scenery.VDICompositorNode
 import graphics.scenery.VolumeManagerManager
 import graphics.scenery.textures.Texture
 import net.imglib2.type.numeric.integer.IntType
+import net.imglib2.type.numeric.real.FloatType
+import org.joml.Matrix4f
 import org.joml.Vector3i
 import org.lwjgl.system.MemoryUtil
 import java.nio.ByteBuffer
 import kotlin.system.measureNanoTime
+import kotlin.math.ceil
 
 class DistributedVDIs(volumeManagerManager: VolumeManagerManager, mpiParameters: MPIParameters)
     : DistributedRenderer(volumeManagerManager, mpiParameters) {
@@ -101,11 +104,45 @@ class DistributedVDIs(volumeManagerManager: VolumeManagerManager, mpiParameters:
             distributeDepthPointer, distributePrefixPointer, mpiPointer)
     }
 
-    override fun uploadForCompositing(buffersToUpload: List<ByteBuffer>, camera: Camera) {
+    override fun uploadForCompositing(cam: Camera, buffersToUpload: List<ByteBuffer>, camera: Camera, colorCounts: IntArray, depthCounts: IntArray) {
         // Upload data for compositing
         val compositor = compositorNode as VDICompositorNode
 
+        if (buffersToUpload.size != 3) {
+            Exception("Expected 3 buffers, got ${buffersToUpload.size}").printStackTrace()
+            return
+        }
+        val vdiSetColour = buffersToUpload[0]
+        val vdiSetDepth = buffersToUpload[1]
+        val prefixSet = buffersToUpload[2]
 
+        val supersegmentsRecvd = (vdiSetColour.remaining() / (4*4)).toFloat() //including potential 0 supersegments that were padded
 
+        logger.debug("Rank: ${mpiParameters.rank}: total supsegs recvd (including 0s): $supersegmentsRecvd")
+
+        for (i in 0 until mpiParameters.commSize) {
+            compositor.totalSupersegmentsFrom[i] = colorCounts[i] / (4 * 4)
+            logger.debug("Rank ${mpiParameters.rank}: totalSupersegmentsFrom $i: ${colorCounts[i] / (4 * 4)}")
+        }
+
+        compositor.material().textures["VDIsColor"] = Texture(Vector3i(512, 512, ceil((supersegmentsRecvd / (512*512)).toDouble()).toInt()), 4, contents = vdiSetColour, usageType = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture),
+            type = FloatType(), mipmap = false, normalized = false, minFilter = Texture.FilteringMode.NearestNeighbour, maxFilter = Texture.FilteringMode.NearestNeighbour)
+
+        compositor.material().textures["VDIsDepth"] = Texture(Vector3i(2 * 512, 512, ceil((supersegmentsRecvd / (512*512)).toDouble()).toInt()), 1, contents = vdiSetDepth, usageType = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture),
+            type = FloatType(), mipmap = false, normalized = false, minFilter = Texture.FilteringMode.NearestNeighbour, maxFilter = Texture.FilteringMode.NearestNeighbour)
+
+        compositor.material().textures["VDIsPrefix"] = Texture(Vector3i(windowHeight, windowWidth, 1), 1, contents = prefixSet, usageType = hashSetOf(Texture.UsageType.LoadStoreImage, Texture.UsageType.Texture),
+            type = IntType(), mipmap = false, minFilter = Texture.FilteringMode.NearestNeighbour, maxFilter = Texture.FilteringMode.NearestNeighbour
+        )
+
+        val view = cam.spatial().getTransformation()
+        compositor.ViewOriginal = view
+        compositor.invViewOriginal = Matrix4f(view).invert()
+
+    }
+
+    override fun setCompositorActivityStatus(setTo: Boolean) {
+        val compositor = compositorNode as VDICompositorNode
+        compositor.doComposite = setTo
     }
 }
