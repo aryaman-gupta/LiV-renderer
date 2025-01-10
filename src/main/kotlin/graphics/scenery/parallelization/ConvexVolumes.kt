73df1727ef498ea99992bfc8a078a3af2cb19302
Aryaman Gupta <aryaman.gupta@tu-dresden.de>
2025-01-10 17:35:40 +0100

    ConvexVolumes: creates implementation of parallelization strategy for simple (convex) distributions

--- src/main/kotlin/graphics/scenery/parallelization/ConvexVolumes.kt
+++ src/main/kotlin/graphics/scenery/parallelization/ConvexVolumes.kt
@@ -1,10 +1,16 @@
 package graphics.scenery.parallelization
 
 import graphics.scenery.VolumeManagerManager
+import java.nio.ByteBuffer
 
 class ConvexVolumes(volumeManagerManager: VolumeManagerManager, mpiParameters: MPIParameters) : ParallelizationBase (volumeManagerManager, mpiParameters) {
 
     override val twoPassRendering = false
+    override val explicitCompositingStep = false
+
+    override fun distributeForCompositing(buffers: List<ByteBuffer>) {
+        // call the ICET composite image function
+    }
 
 
 }
