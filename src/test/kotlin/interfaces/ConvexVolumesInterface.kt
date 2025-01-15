918c9b8a5d9b43bb9cb8e6685614d98e0e5957fc
Aryaman Gupta <aryaman.gupta@tu-dresden.de>
2025-01-15 16:52:47 +0100

    ConvexVolumesInterface: creates interface for compositing of convex domain decompositions

---
+++ src/test/kotlin/interfaces/ConvexVolumesInterface.kt
@@ -0,0 +1,9 @@
+package graphics.scenery.tests.interfaces
+
+import graphics.scenery.interfaces.RenderingInterfaceBase
+
+class ConvexVolumesInterface (wWidth: Int, wHeight: Int) : RenderingInterfaceBase("ConvexVolumes", wWidth, wHeight) {
+
+    override fun setupVolumeManagerManager() {
+    }
+}
