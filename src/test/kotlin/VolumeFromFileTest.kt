68a1d7f15e9f92cd2052a9bc317d1c0ead2f7475
Aryaman Gupta <argupta@mpi-cbg.de>
2025-10-07 13:41:12 +0530

    VolumeFromFileTest: adds simple test to render volume loaded from file

---
+++ src/test/kotlin/VolumeFromFileTest.kt
@@ -0,0 +1,64 @@
+package graphics.scenery.tests
+
+import graphics.scenery.tests.interfaces.ConvexVolumesInterface
+import graphics.scenery.tests.interfaces.NaiveInterface
+import graphics.scenery.tests.interfaces.VDIGenerationInterface
+import graphics.scenery.volumes.TransferFunction
+import java.io.File
+import java.nio.ByteBuffer
+import kotlin.concurrent.thread
+
+
+fun main(args: Array<String>) {
+    if (args.size < 5) {
+        println("Usage: <program> <data file path> <dimX> <dimY> <dimZ> <is16bit?>")
+        return
+    }
+
+    val dataFilePath = args[0]
+    val volumeDims = intArrayOf(args[1].toInt(), args[2].toInt(), args[3].toInt())
+    val is16bit = args[4].toBoolean()
+
+    val instance = VDIGenerationInterface(512, 512, 0, 1, 0)
+
+    thread {
+        instance.main()
+    }
+
+    instance.waitRendererReady()
+
+    instance.setVolumeDimensions(volumeDims)
+
+    val scaling = instance.getVolumeScaling()
+
+//    instance.addProcessorData(0, floatArrayOf(0f, 0f, 0f), volumeDims.map { it.toFloat() }.toFloatArray())
+
+    // Add a volume
+    val volumePosition = floatArrayOf(0.0f, 0.0f, 0.0f)
+
+    instance.addVolume(0, volumeDims, volumePosition, is16bit)
+
+    // Create a buffer to update the volume
+    val bufferSize = volumeDims[0] * volumeDims[1] * volumeDims[2] * if (is16bit) 2 else 1
+    val buffer = ByteBuffer.allocateDirect(bufferSize)
+
+    // Read the data from the file
+    val file = File(dataFilePath)
+    val fileData = file.readBytes()
+    buffer.put(fileData).flip()
+
+    // Update the volume
+    instance.updateVolume(0, buffer)
+
+//    instance.setTransferFunction(0, TransferFunction.ramp(0.1f, 0.5f))
+
+
+    instance.sceneSetupComplete.set(true)
+
+    // Start the rendering loop
+    while (true) {
+        // Perform rendering tasks
+        Thread.sleep(1000000)
+        instance.stopRendering()
+    }
+}
